95b2386233285734ae8e0db11e773da4ee2bfb5d
SimphiweThwala <simphiwelhs@gmail.com>
2020-11-05 19:27:08 +0200

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CMPG_313.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
